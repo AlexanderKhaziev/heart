0f913617fc36b91cd5b0aa953b3e5315592ddff0
AlexanderKhaziev <53128345+AlexanderKhaziev@users.noreply.github.com>
2020-11-28 17:43:40 +0500

    1st

---
+++ umka/src/main/java/ru/greenhack/umka/UmkaApplication.java
@@ -0,0 +1,13 @@
+package ru.greenhack.umka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmkaApplication.class, args);
+	}
+
+}
---
+++ umka/src/test/java/ru/greenhack/umka/UmkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.greenhack.umka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
